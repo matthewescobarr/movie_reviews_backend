6fed7a585df4bea84051c4de328d807717b272fd
Matthew Escobar <mde27@njit.edu>
2026-09-21 12:00:21 -0400

    Completed 09.21 Clas Lecture

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ import app from './server.js'
 import mongodb from "mongodb"
 import dotenv from "dotenv"
 import MoviesDAO from './dao/moviesDAO.js'
+import ReviewsDAO from './dao/reviewsDAO.js'
 
 async function main() {
 
   dotenv.config()
 
-  const client = new mongodb.MongoClient( process.env.MOVIEREVIEWS_DB_URI)
+  const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI)
 
   const port = process.env.PORT || 8000
 
   try {
     await client.connect()
     await MoviesDAO.injectDB(client)
+    await ReviewsDAO.injectDB(client)
+
     app.listen(port, () => {
-    console.log('server is running on port: ' + port);
+      console.log('server is running on port: ' + port);
     })
 
   } catch (e) {
